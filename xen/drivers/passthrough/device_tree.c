2631a762725db364299715cc005b5db553599dd3
Andrii Anisov <andrii_anisov@epam.com>
2017-02-03 19:15:06 +0200

    fix the print to reflect the domctl really executed

--- xen/drivers/passthrough/device_tree.c
+++ xen/drivers/passthrough/device_tree.c
@@ -187,7 +187,7 @@ int iommu_do_dt_domctl(struct xen_domctl *domctl, struct domain *d,
         ret = iommu_deassign_dt_device(d, dev);
 
         if ( ret )
-            printk(XENLOG_G_ERR "XEN_DOMCTL_assign_dt_device: assign \"%s\""
+            printk(XENLOG_G_ERR "XEN_DOMCTL_deassign_dt_device: assign \"%s\""
                    " to dom%u failed (%d)\n",
                    dt_node_full_name(dev), d->domain_id, ret);
         break;
