1090b834ce7f6aafccfb01185862558f4442a1e1
Andrii Anisov <andrii_anisov@epam.com>
2019-02-06 19:55:11 +0200

    HACK:xen: keep runstate area mapped
    This is the patch from Roger Pau Monne.
    The patch does map runstate area once, so that there is no need to
    map that area each time hypervisor updates it.
    This patch is rather controversial, there are doubts if VM could
    change its runstate mappings without hypervisor notification.
    As long as we still have no decission, no code cleanup is made as
    well. See details here [1].
    
    [1] https://lists.xenproject.org/archives/html/xen-devel/2019-02/msg00416.html
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/include/asm-x86/domain.h
+++ xen/include/asm-x86/domain.h
@@ -540,7 +540,6 @@ struct pv_vcpu
     uint32_t dr7_emul;
 
     /* Deferred VA-based update state. */
-    bool_t need_update_runstate_area;
     struct vcpu_time_info pending_system_time;
 };
 
@@ -632,7 +631,6 @@ struct guest_memory_policy
 void update_guest_memory_policy(struct vcpu *v,
                                 struct guest_memory_policy *policy);
 
-bool update_runstate_area(struct vcpu *);
 bool update_secondary_system_time(struct vcpu *,
                                   struct vcpu_time_info *);
 
