905fe5da1bfb71f27bd2b47a876f025b79d0a96d
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2020-06-17 23:17:28 +0300

    Revert "libxl: Add DTB passthrough nodes list"
    This reverts commit b52d7e9f673cbd6062f649a16c835092c99a79ca.

--- tools/libxl/libxl_arm.c
+++ tools/libxl/libxl_arm.c
@@ -843,10 +843,9 @@ static int copy_node_by_path(libxl__gc *gc, const char *path,
  *  - /passthrough node
  *  - /aliases node
  */
-static int copy_partial_fdt(libxl__gc *gc, void *fdt, void *pfdt,
-                            const libxl_domain_build_info *info)
+static int copy_partial_fdt(libxl__gc *gc, void *fdt, void *pfdt)
 {
-    int i, r;
+    int r;
 
     r = copy_node_by_path(gc, "/passthrough", fdt, pfdt);
     if (r < 0) {
@@ -858,16 +857,6 @@ static int copy_partial_fdt(libxl__gc *gc, void *fdt, void *pfdt,
     if (r < 0 && r != -FDT_ERR_NOTFOUND) {
         LOG(ERROR, "Can't copy the node \"/aliases\" from the partial FDT");
         return r;
-    }
-
-    for (i = 0; i < libxl_string_list_length(&info->dt_passthrough_nodes);
-         i++) {
-        r = copy_node_by_path(gc, info->dt_passthrough_nodes[i], fdt, pfdt);
-        if (r < 0 && r != -FDT_ERR_NOTFOUND) {
-            LOG(ERROR, "Can't copy the node \"%s\" from the partial FDT",
-                info->dt_passthrough_nodes[i]);
-            return r;
-        }
     }
 
     return 0;
@@ -882,8 +871,7 @@ static int check_partial_fdt(libxl__gc *gc, void *fdt, size_t size)
     return ERROR_FAIL;
 }
 
-static int copy_partial_fdt(libxl__gc *gc, void *fdt, void *pfdt,
-                            const libxl_domain_build_info *info)
+static int copy_partial_fdt(libxl__gc *gc, void *fdt, void *pfdt)
 {
     /*
      * We should never be here when the partial device tree is not
@@ -1009,7 +997,7 @@ next_resize:
             FDT( make_optee_node(gc, fdt) );
 
         if (pfdt)
-            FDT( copy_partial_fdt(gc, fdt, pfdt, info) );
+            FDT( copy_partial_fdt(gc, fdt, pfdt) );
 
         FDT( fdt_end_node(fdt) );
 
