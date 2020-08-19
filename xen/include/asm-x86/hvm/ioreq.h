0cfea49c6e17e0233165d15164488479cec3d55c
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2020-08-19 14:21:51 +0300

    xen/ioreq: Make x86's IOREQ feature common
    As a lot of x86 code can be re-used on Arm later on, this patch
    moves previously prepared IOREQ support to the common code.
    
    The code movement is almost a verbatim copy with re-ordering
    the headers alphabetically.
    
    This support is going to be used on Arm to be able run device
    emulator outside of Xen hypervisor.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    
    ---
    Please note, this is a split/cleanup/hardening of Julien's PoC:
    "Add support for Guest IO forwarding to a device emulator"
    
    Changes RFC -> V1:
       - was split into three patches:
         - x86/ioreq: Prepare IOREQ feature for making it common
         - xen/ioreq: Make x86's IOREQ feature common
         - xen/ioreq: Make x86's hvm_ioreq_needs_completion() common
       - update MAINTAINERS file
       - do not use a separate subdir for the IOREQ stuff, move it to:
         - xen/common/ioreq.c
         - xen/include/xen/ioreq.h
       - update x86's files to include xen/ioreq.h
       - remove unneeded headers in arch/x86/hvm/ioreq.c
       - re-order the headers alphabetically in common/ioreq.c
       - update common/ioreq.c according to the newly introduced arch functions:
         arch_hvm_destroy_ioreq_server()/arch_handle_hvm_io_completion()
    ---

--- xen/include/asm-x86/hvm/ioreq.h
+++ xen/include/asm-x86/hvm/ioreq.h
@@ -19,41 +19,12 @@
 #ifndef __ASM_X86_HVM_IOREQ_H__
 #define __ASM_X86_HVM_IOREQ_H__
 
-bool hvm_io_pending(struct vcpu *v);
-bool handle_hvm_io_completion(struct vcpu *v);
-bool is_ioreq_server_page(struct domain *d, const struct page_info *page);
+#include <asm/hvm/emulate.h>
+#include <asm/hvm/hvm.h>
+#include <asm/hvm/vmx/vmx.h>
 
-int hvm_create_ioreq_server(struct domain *d, int bufioreq_handling,
-                            ioservid_t *id);
-int hvm_destroy_ioreq_server(struct domain *d, ioservid_t id);
-int hvm_get_ioreq_server_info(struct domain *d, ioservid_t id,
-                              unsigned long *ioreq_gfn,
-                              unsigned long *bufioreq_gfn,
-                              evtchn_port_t *bufioreq_port);
-int hvm_get_ioreq_server_frame(struct domain *d, ioservid_t id,
-                               unsigned long idx, mfn_t *mfn);
-int hvm_map_io_range_to_ioreq_server(struct domain *d, ioservid_t id,
-                                     uint32_t type, uint64_t start,
-                                     uint64_t end);
-int hvm_unmap_io_range_from_ioreq_server(struct domain *d, ioservid_t id,
-                                         uint32_t type, uint64_t start,
-                                         uint64_t end);
 int hvm_map_mem_type_to_ioreq_server(struct domain *d, ioservid_t id,
                                      uint32_t type, uint32_t flags);
-int hvm_set_ioreq_server_state(struct domain *d, ioservid_t id,
-                               bool enabled);
-
-int hvm_all_ioreq_servers_add_vcpu(struct domain *d, struct vcpu *v);
-void hvm_all_ioreq_servers_remove_vcpu(struct domain *d, struct vcpu *v);
-void hvm_destroy_all_ioreq_servers(struct domain *d);
-
-struct hvm_ioreq_server *hvm_select_ioreq_server(struct domain *d,
-                                                 ioreq_t *p);
-int hvm_send_ioreq(struct hvm_ioreq_server *s, ioreq_t *proto_p,
-                   bool buffered);
-unsigned int hvm_broadcast_ioreq(ioreq_t *p, bool buffered);
-
-void hvm_ioreq_init(struct domain *d);
 
 int arch_hvm_destroy_ioreq_server(struct hvm_ioreq_server *s);
 
