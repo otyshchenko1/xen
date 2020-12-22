9741fa92e6ff4c707b6e10673a27f79c09d39494
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2020-12-22 21:21:16 +0200

    [WIP] iommu/ipmmu-vmsa: Implement missing PCI bits
    The most unclear moment is "devfn != pdev->devfn" condition
    in arch hooks.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/drivers/passthrough/arm/ipmmu-vmsa.c
+++ xen/drivers/passthrough/arm/ipmmu-vmsa.c
@@ -741,8 +741,8 @@ static void ipmmu_detach_device(struct ipmmu_vmsa_domain *domain,
         ipmmu_utlb_disable(domain, fwspec->ids[i]);
 }
 
-static int ipmmu_init_platform_device(struct device *dev,
-                                      const struct dt_phandle_args *args)
+static int ipmmu_init_device(struct device *dev,
+                             const struct dt_phandle_args *args)
 {
     struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
     struct ipmmu_vmsa_device *mmu;
@@ -1080,22 +1080,6 @@ static void ipmmu_free_root_domain(struct ipmmu_vmsa_domain *domain)
     xfree(domain);
 }
 
-static int reassign_device(struct domain *source, struct domain *target,
-                           u8 devfn, struct pci_dev *pdev)
-{
-    if ( devfn == pdev->devfn )
-    {
-        list_move(&pdev->domain_list, &target->pdev_list);
-        pdev->domain = target;
-    }
-
-    printk(XENLOG_INFO "Re-assign %04x:%02x:%02x.%u from dom%d to dom%d\n",
-           pdev->seg, pdev->bus, PCI_SLOT(devfn), PCI_FUNC(devfn),
-           source->domain_id, target->domain_id);
-
-    return 0;
-}
-
 static int ipmmu_assign_device(struct domain *d, u8 devfn, struct device *dev,
                                uint32_t flag)
 {
@@ -1106,15 +1090,31 @@ static int ipmmu_assign_device(struct domain *d, u8 devfn, struct device *dev,
     if ( !xen_domain )
         return -EINVAL;
 
+    if ( !to_ipmmu(dev) )
+        return -ENODEV;
+
     if ( dev_is_pci(dev) )
     {
         struct pci_dev *pdev = dev_to_pci(dev);
 
-        return reassign_device(pdev->domain, d, devfn, pdev);
-    }
+        printk(XENLOG_INFO "Assigning device %04x:%02x:%02x.%u to dom%d\n",
+               pdev->seg, pdev->bus, PCI_SLOT(devfn), PCI_FUNC(devfn),
+               d->domain_id);
 
-    if ( !to_ipmmu(dev) )
-        return -ENODEV;
+        /*
+         * XXX What would be the proper behavior? This could happen if
+         * pdev->phantom_stride > 0
+         */
+        if ( devfn != pdev->devfn )
+            ASSERT_UNREACHABLE();
+
+        list_move(&pdev->domain_list, &d->pdev_list);
+        pdev->domain = d;
+
+        /* dom_io is used as a sentinel for quarantined devices */
+        if ( d == dom_io )
+            return 0;
+    }
 
     spin_lock(&xen_domain->lock);
 
@@ -1187,10 +1187,30 @@ out:
     return ret;
 }
 
-static int ipmmu_deassign_device(struct domain *d, struct device *dev)
+static int ipmmu_deassign_device(struct domain *d, u8 devfn, struct device *dev)
 {
     struct ipmmu_vmsa_xen_domain *xen_domain = dom_iommu(d)->arch.priv;
     struct ipmmu_vmsa_domain *domain = to_domain(dev);
+
+    if ( dev_is_pci(dev) )
+    {
+        struct pci_dev *pdev = dev_to_pci(dev);
+
+        printk(XENLOG_INFO "Deassigning device %04x:%02x:%02x.%u from dom%d\n",
+               pdev->seg, pdev->bus, PCI_SLOT(devfn), PCI_FUNC(devfn),
+               d->domain_id);
+
+        /*
+         * XXX What would be the proper behavior? This could happen if
+         * pdev->phantom_stride > 0
+         */
+        if ( devfn != pdev->devfn )
+            ASSERT_UNREACHABLE();
+
+        /* dom_io is used as a sentinel for quarantined devices */
+        if ( d == dom_io )
+            return 0;
+    }
 
     if ( !domain || domain->d != d )
     {
@@ -1228,7 +1248,7 @@ static int ipmmu_reassign_device(struct domain *s, struct domain *t,
     if ( t == s )
         return 0;
 
-    ret = ipmmu_deassign_device(s, dev);
+    ret = ipmmu_deassign_device(s, devfn, dev);
     if ( ret )
         return ret;
 
@@ -1249,9 +1269,10 @@ static int ipmmu_dt_xlate(struct device *dev,
     int ret;
 
     /*
-     * Perform sanity check of passed DT IOMMU specifier. Each master device
-     * gets micro-TLB (device ID) assignment via the "iommus" property
-     * in DT. We expect #iommu-cells to be 1 (Multiple-master IOMMU) and
+     * Perform sanity check of passed DT IOMMU specifier. Each device gets
+     * micro-TLB (device ID) assignment either via the "iommus" property
+     * (platform device) or via the "iommu-map" property (PCI device) in DT.
+     * We expect #iommu-cells to be 1 (Multiple-master IOMMU) and
      * this cell for the micro-TLB (device ID).
      */
     if ( spec->args_count != 1 || spec->args[0] >= IPMMU_UTLB_MAX )
@@ -1265,7 +1286,7 @@ static int ipmmu_dt_xlate(struct device *dev,
     if ( to_ipmmu(dev) )
         return 0;
 
-    return ipmmu_init_platform_device(dev, spec);
+    return ipmmu_init_device(dev, spec);
 }
 
 static int ipmmu_add_device(u8 devfn, struct device *dev)
@@ -1276,19 +1297,52 @@ static int ipmmu_add_device(u8 devfn, struct device *dev)
     if ( !to_ipmmu(dev) )
         return -ENODEV;
 
-    if ( dt_device_is_protected(dev_to_dt(dev)) )
+    /*
+     * TODO Optimize code below.
+     * Can we have the single is_protected flag in struct device?
+     */
+    if ( !dev_is_pci(dev) )
     {
-        dev_err(dev, "Already added to IPMMU\n");
-        return -EEXIST;
-    }
+        if ( dt_device_is_protected(dev_to_dt(dev)) )
+        {
+            dev_err(dev, "Already added to IPMMU\n");
+            return -EEXIST;
+        }
 
-    /* Let Xen know that the master device is protected by an IOMMU. */
-    dt_device_set_protected(dev_to_dt(dev));
+        /* Let Xen know that the master device is protected by an IOMMU. */
+        dt_device_set_protected(dev_to_dt(dev));
+    }
+    else
+    {
+        struct pci_dev *pdev = dev_to_pci(dev);
+
+        /*
+         * XXX What would be the proper behavior? This could happen if
+         * pdev->phantom_stride > 0
+         */
+        if ( devfn != pdev->devfn )
+            ASSERT_UNREACHABLE();
+
+        if ( pci_device_is_protected(pdev) )
+        {
+            dev_err(dev, "Already added to IPMMU\n");
+            return -EEXIST;
+        }
+
+        /* Let Xen know that the master device is protected by an IOMMU. */
+        pci_device_set_protected(pdev);
+    }
 
     dev_info(dev, "Added master device (IPMMU %s micro-TLBs %u)\n",
              dev_name(fwspec->iommu_dev), fwspec->num_ids);
 
     return 0;
+}
+
+static int ipmmu_remove_device(u8 devfn, struct device *dev)
+{
+    /* XXX Implement me */
+    ASSERT_UNREACHABLE();
 }
 
 static int ipmmu_iommu_domain_init(struct domain *d)
@@ -1363,6 +1417,7 @@ static const struct iommu_ops ipmmu_iommu_ops =
     .unmap_page      = arm_iommu_unmap_page,
     .dt_xlate        = ipmmu_dt_xlate,
     .add_device      = ipmmu_add_device,
+    .remove_device   = ipmmu_remove_device,
 };
 
 static const struct dt_device_match ipmmu_dt_match[] __initconst =
