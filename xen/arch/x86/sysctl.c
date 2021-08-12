718e921818881e5952662b5a1c8e8121d14aba84
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2021-08-12 21:30:29 +0300

    xen: Introduce "gpaddr_bits" field to XEN_SYSCTL_physinfo
    We need to pass info about maximum supported guest address
    space size to the toolstack on Arm in order to properly
    calculate the base and size of the extended region (safe range)
    for the guest. The extended region is unused address space which
    could be safely used by domain for foreign/grant mappings on Arm.
    The extended region itself will be handled by the subsequents
    patch.
    
    Use p2m_ipa_bits variable on Arm, the x86 equivalent is
    hap_paddr_bits.
    
    As we change the size of structure bump the interface version.
    
    Suggested-by: Julien Grall <jgrall@amazon.com>
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    ---
    Changes since RFC:
       - update patch subject/description
       - replace arch-specific sub-struct with common gpaddr_bits
         field and update code to reflect that
    ---

--- xen/arch/x86/sysctl.c
+++ xen/arch/x86/sysctl.c
@@ -135,6 +135,8 @@ void arch_do_physinfo(struct xen_sysctl_physinfo *pi)
         pi->capabilities |= XEN_SYSCTL_PHYSCAP_hap;
     if ( IS_ENABLED(CONFIG_SHADOW_PAGING) )
         pi->capabilities |= XEN_SYSCTL_PHYSCAP_shadow;
+
+    pi->gpaddr_bits = hap_paddr_bits;
 }
 
 long arch_do_sysctl(
