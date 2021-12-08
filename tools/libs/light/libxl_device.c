896ddb20be2612503441428d27353a5acb8b4775
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2021-12-08 14:54:55 +0200

    [UPDATED] libxl: Add support for Virtio disk configuration
    This patch adds basic support for configuring and assisting virtio-disk
    backend (emualator) which is intended to run out of Qemu and could be
    run in any domain.
    Although the Virtio block device is quite different from traditional
    Xen PV block device (vbd) from the toolstack point of view:
     - as the frontend is virtio-blk which is not a Xenbus driver, nothing
       written to Xenstore are fetched by the frontend (the vdev is not
       passed to the frontend)
     - the ring-ref/event-channel are not used for the backend<->frontend
       communication, the proposed IPC for Virtio is IOREQ/DM
    it is still a "block device" and ought to be integrated in existing
    "disk" handling. So, re-use (and adapt) "disk" parsing/configuration
    logic to deal with Virtio devices as well.
    
    Besides introducing new disk backend type (LIBXL_DISK_BACKEND_VIRTIO)
    introduce new device kind (LIBXL__DEVICE_KIND_VIRTIO_DISK) as current
    one (LIBXL__DEVICE_KIND_VBD) doesn't fit into Virtio disk model.
    
    In order to inform the toolstack that Virtio disk needs to be used
    extend "disk" configuration by introducing new "virtio" flag.
    An example of domain configuration:
    disk = [ 'backend=DomD, phy:/dev/mmcblk1p3, xvda1, rw, virtio' ]
    
    Please note, this patch is not enough for virtio-disk to work
    on Xen (Arm), as for every Virtio device (including disk) we need
    to allocate Virtio MMIO params (IRQ and memory region) and pass
    them to the backend, also update Guest device-tree. The subsequent
    patch will add these missing bits. For the current patch,
    the default "irq" and "base" are just written to the Xenstore.
    
    ***** UPDATE! *****
    
    The separate flag "virtio" was changed to new backendtype:
    disk = [ 'backend=DomD, phy:/dev/mmcblk1p3, xvda1, rw, backendtype=virtio' ]
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    
    ---
    Changes RFC -> V1:
       - no changes
    
    Changes V1 -> V2:
       - rebase according to the new location of libxl_virtio_disk.c
    
    Changes V2 -> V3:
       - no changes
    
    Changes V3 -> V4:
       - rebase according to the new argument for DEFINE_DEVICE_TYPE_STRUCT
    
    Changes V4 -> V5:
       - split the changes, change the order of the patches
       - update patch description
       - don't introduce new "vdisk" configuration option with own parsing logic,
         re-use Xen PV block "disk" parsing/configuration logic for the virtio-disk
       - introduce "virtio" flag and document it's usage
       - add LIBXL_HAVE_DEVICE_DISK_VIRTIO
       - update libxlu_disk_l.[ch]
       - drop num_disks variable/MAX_VIRTIO_DISKS
       - drop Wei's T-b
    
    Changes V5 -> V6:
       - rebase on current staging
       - use "%"PRIu64 instead of %lu for disk->base in device_disk_add()
       - update *.gen.go files
    ---

--- tools/libs/light/libxl_device.c
+++ tools/libs/light/libxl_device.c
@@ -292,6 +292,9 @@ static int disk_try_backend(disk_try_backend_args *a,
     /* returns 0 (ie, DISK_BACKEND_UNKNOWN) on failure, or
      * backend on success */
 
+    assert((a->disk->virtio && backend == LIBXL_DISK_BACKEND_VIRTIO) ||
+           (!a->disk->virtio && backend != LIBXL_DISK_BACKEND_VIRTIO));
+
     switch (backend) {
     case LIBXL_DISK_BACKEND_PHY:
         if (a->disk->format != LIBXL_DISK_FORMAT_RAW) {
@@ -328,6 +331,29 @@ static int disk_try_backend(disk_try_backend_args *a,
     case LIBXL_DISK_BACKEND_QDISK:
         if (a->disk->script) goto bad_script;
         return backend;
+
+    case LIBXL_DISK_BACKEND_VIRTIO:
+        if (a->disk->format != LIBXL_DISK_FORMAT_RAW)
+            goto bad_format;
+
+        if (a->disk->script)
+            goto bad_script;
+
+        if (libxl_defbool_val(a->disk->colo_enable))
+            goto bad_colo;
+
+        if (a->disk->backend_domid != LIBXL_TOOLSTACK_DOMID) {
+            LOG(DEBUG, "Disk vdev=%s, is using a storage driver domain, "
+                       "skipping physical device check", a->disk->vdev);
+            return backend;
+        }
+
+        if (libxl__try_phy_backend(a->stab.st_mode))
+            return backend;
+
+        LOG(DEBUG, "Disk vdev=%s, backend virtio unsuitable as phys path not a "
+                   "block device", a->disk->vdev);
+        return 0;
 
     default:
         LOG(DEBUG, "Disk vdev=%s, backend %d unknown", a->disk->vdev, backend);
@@ -392,7 +418,8 @@ int libxl__device_disk_set_backend(libxl__gc *gc, libxl_device_disk *disk) {
         }
         memset(&a.stab, 0, sizeof(a.stab));
     } else if ((disk->backend == LIBXL_DISK_BACKEND_UNKNOWN ||
-                disk->backend == LIBXL_DISK_BACKEND_PHY) &&
+                disk->backend == LIBXL_DISK_BACKEND_PHY ||
+                disk->backend == LIBXL_DISK_BACKEND_VIRTIO) &&
                disk->backend_domid == LIBXL_TOOLSTACK_DOMID &&
                !disk->script) {
         if (stat(disk->pdev_path, &a.stab)) {
@@ -408,7 +435,8 @@ int libxl__device_disk_set_backend(libxl__gc *gc, libxl_device_disk *disk) {
         ok=
             disk_try_backend(&a, LIBXL_DISK_BACKEND_PHY) ?:
             disk_try_backend(&a, LIBXL_DISK_BACKEND_QDISK) ?:
-            disk_try_backend(&a, LIBXL_DISK_BACKEND_TAP);
+            disk_try_backend(&a, LIBXL_DISK_BACKEND_TAP) ?:
+            disk_try_backend(&a, LIBXL_DISK_BACKEND_VIRTIO);
         if (ok)
             LOG(DEBUG, "Disk vdev=%s, using backend %s",
                        disk->vdev,
@@ -441,6 +469,7 @@ char *libxl__device_disk_string_of_backend(libxl_disk_backend backend)
         case LIBXL_DISK_BACKEND_QDISK: return "qdisk";
         case LIBXL_DISK_BACKEND_TAP: return "phy";
         case LIBXL_DISK_BACKEND_PHY: return "phy";
+        case LIBXL_DISK_BACKEND_VIRTIO: return "virtio_disk";
         default: return NULL;
     }
 }
