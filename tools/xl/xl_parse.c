92e46aa48d306b08a0283fdaa082d6b38e33e42e
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-01-31 13:40:57 +0200

    libxl/arm: Add basic virtio-iommu support
    Introduce "virtio-iommu" config option (disabled by default) which
    purpose is to indicate that virtio devices which serve a particular
    guest are behind the IOMMU.
    If set, generate virtio-iommu DT node and link all virtio devices
    to it. We are going to use scenario where all virtio devices inside
    a guest are connected to the same virtio-iommu unit (share the IOVA
    space).
    
    The virtio-iommu parameters (enable, MMIO irq and base) are passed
    to the hypervisor via XEN_DOMCTL_createdomain using sub-arch fields.
    
    The following setting must be enabled in the guest Linux defconfig:
    CONFIG_VIRTIO_IOMMU=y
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- tools/xl/xl_parse.c
+++ tools/xl/xl_parse.c
@@ -2925,6 +2925,8 @@ skip_usbdev:
 
     xlu_cfg_get_defbool(config, "dm_restrict", &b_info->dm_restrict, 0);
 
+    xlu_cfg_get_defbool(config, "virtio_iommu", &b_info->arch_arm.viommu_enable, 0);
+
     if (c_info->type == LIBXL_DOMAIN_TYPE_HVM) {
         if (!xlu_cfg_get_string (config, "vga", &buf, 0)) {
             if (!strcmp(buf, "stdvga")) {
--- xen/include/public/arch-arm.h
+++ xen/include/public/arch-arm.h
@@ -317,15 +317,29 @@ DEFINE_XEN_GUEST_HANDLE(vcpu_guest_context_t);
 struct xen_arch_domainconfig {
     /* IN/OUT */
     uint8_t gic_version;
+
     /* IN */
-    uint16_t tee_type;
+    uint8_t viommu_enable;
+
     /* IN */
-    uint32_t nr_spis;
+    uint8_t tee_type;
+
     /*
      * IN
      * OSID used by virtual GSX device.
      */
     uint8_t vgsx_osid;
+
+    /*
+     * IN
+     * virtio-mmio specific data for the virtio-iommu backend
+     */
+    uint32_t viommu_irq;
+    uint64_t viommu_base;
+
+    /* IN */
+    uint32_t nr_spis;
+
     /*
      * OUT
      * Based on the property clock-frequency in the DT timer node.
@@ -501,6 +515,8 @@ typedef uint64_t xen_callback_t;
 #define PSCI_cpu_off     1
 #define PSCI_cpu_on      2
 #define PSCI_migrate     3
+
+#define GUEST_VIRTIO_MMIO_IOMMU_ID   0x10000
 
 #endif
 
