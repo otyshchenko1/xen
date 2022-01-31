ca584a82dd3c799370ef0b8abb65dbfdbd1001b8
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-01-31 13:40:31 +0200

    xen/lib: Update rbtree code
    Align Xen code with what was found in Linux 5.16-rc3, also pull
    various enhancements such as interval tree, cached rbtrees, etc.
    
    Mostly a verbatim copy with some tweaks:
    1. Replace "__always_inline" with "always_inline" and "WRITE_ONCE"
       with "ACCESS_ONCE"
    2. Drop pointless "EXPORT_SYMBOL()"
    3. Include Xen headers
    
    This will be needed for the subsequent patches that introduce
    virtio-iommu device to Xen.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

---
+++ xen/lib/interval_tree.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#include <xen/interval_tree.h>
+#include <xen/interval_tree_generic.h>
+
+#define START(node) ((node)->start)
+#define LAST(node)  ((node)->last)
+
+INTERVAL_TREE_DEFINE(struct interval_tree_node, rb,
+		     unsigned long, __subtree_last,
+		     START, LAST,, interval_tree)
