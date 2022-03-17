3e1d8f921d5fc192c9e8d0900fabe3b9f774cdbc
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2022-03-17 10:31:31 +0200

    temp
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- config/StdGNU.mk
+++ config/StdGNU.mk
@@ -1,15 +1,15 @@
 AS         = $(CROSS_COMPILE)as
-LD         = $(CROSS_COMPILE)ld
+LD         ?= $(CROSS_COMPILE)ld
 ifeq ($(clang),y)
-CC         = $(CROSS_COMPILE)clang
-CXX        = $(CROSS_COMPILE)clang++
+CC         ?= $(CROSS_COMPILE)clang
+CXX        ?= $(CROSS_COMPILE)clang++
 LD_LTO     = $(CROSS_COMPILE)llvm-ld
 else
-CC         = $(CROSS_COMPILE)gcc
-CXX        = $(CROSS_COMPILE)g++
+CC         ?= $(CROSS_COMPILE)gcc
+CXX        ?= $(CROSS_COMPILE)g++
 LD_LTO     = $(CROSS_COMPILE)ld
 endif
-CPP        = $(CC) -E
+CPP        ?= $(CC) -E
 ADDR2LINE  = $(CROSS_COMPILE)addr2line
 AR         = $(CROSS_COMPILE)ar
 RANLIB     = $(CROSS_COMPILE)ranlib
