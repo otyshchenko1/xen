83d6893e91f1402892470b46f86e99a0f08a18a1
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2023-07-10 18:03:13 +0300

    !FIXUP! with "vpci: add hooks for PCI device assign/de-assign"
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/drivers/passthrough/pci.c
+++ xen/drivers/passthrough/pci.c
@@ -1461,7 +1461,6 @@ static int assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag)
 {
     const struct domain_iommu *hd = dom_iommu(d);
     struct pci_dev *pdev;
-    uint8_t old_devfn;
     int rc = 0;
 
     if ( !is_iommu_enabled(d) )
@@ -1500,8 +1499,6 @@ static int assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag)
                           pci_to_dev(pdev), flag)) )
         goto done;
 
-    old_devfn = devfn;
-
     for ( ; pdev->phantom_stride; rc = 0 )
     {
         devfn += pdev->phantom_stride;
@@ -1510,10 +1507,17 @@ static int assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag)
         rc = iommu_call(hd->platform_ops, assign_device, d, devfn,
                         pci_to_dev(pdev), flag);
     }
+    if ( rc )
+        goto done;
 
+    devfn = pdev->devfn;
     rc = vpci_assign_device(pdev);
-    if ( rc && deassign_device(d, seg, bus, old_devfn) )
+    if ( rc && deassign_device(d, seg, bus, devfn) )
+    {
+        printk(XENLOG_ERR "%pd: %pp was left partially assigned\n",
+               d, &PCI_SBDF(seg, bus, devfn));
         domain_crash(d);
+    }
 
  done:
     if ( rc )
